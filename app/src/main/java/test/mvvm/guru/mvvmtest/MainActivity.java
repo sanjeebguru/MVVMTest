1fd90be5296bb9f3def48b29ff4630142a6b2d9c
Sanjeeb Guru <guru.sanjeeb@gmail.com>
2022-03-04 19:52:00 +0530

    commit in master

--- app/src/main/java/test/mvvm/guru/mvvmtest/MainActivity.java
+++ app/src/main/java/test/mvvm/guru/mvvmtest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //test commit
         //commit form master
+        //commit in the master branch
     }
 }
